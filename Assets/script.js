b1c4740ddfb5c80525b8764e9f4cef18536b6974
Kaitlyn Diaz <katediaz011@gmail.com>
2022-03-31 22:55:00 -0400

    working on randomselected function

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+var generateBtn = document.querySelector("#generate");
+
 // Arrays for special characters, lowercase letters, uppercase letters, and numbers
 const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
 const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -7,11 +9,14 @@ const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 // need a function to get password selected by user 
 
 var passwordLength;
+
+
  
 
 function userSelected () {
       var userChoices = [];
-      var passwordLength = window.prompt ("How many characters would you like your password to contain?")
+      var noCounter = 0;
+      passwordLength = window.prompt ("How many characters would you like your password to contain?");
 // error handling for amount of chracters in password
   if (passwordLength >= 8 && passwordLength <= 128) { 
  // confirmation for user choices through confirm method 
@@ -19,25 +24,30 @@ function userSelected () {
    return;
 
   } if (window.confirm ("Click OK if you want to include special characters.")) {
-    window.alert("You will have special characters.")); userChoices.concat(specialChars);
-
-  } if (window.confirm ("Click OK to confirm if you want to include numeric characters.")) {
-    window.alert("You will have numeric characters."); userChoices.concat(numberChars);
-  } if (window.confirm ("Click OK to confirm if you want to include uppercase characters.")) {
-    window.alert ("You will have uppercase characters."); userChoices.concat(uppercase);
-  } if (window.confirm ("Click OK to confirm if you want to include lowercase characters.")) {
-    window.alert("You will have lowercase characters."); userChoices.concat(lowercase);
+    userChoices.concat(specialChars);
+  } else {
+    noCounter++;
   }
-  // if statement for the user choosing no throughout the selection
-   if (
-    (window.confirm ("Click OK if you want to include special characters.")) === false &&
-    (window.confirm ("Click OK to confirm if you want to include numeric characters.")) === false &&
-    (window.confirm ("Click OK to confirm if you want to include uppercase characters.")) === false &&
-    (window.confirm ("Click OK to confirm if you want to include lowercase characters.")) === false
-    ) { window.alert("You must choose at least one Character."); 
-    return;}
-    
-  
+    if (window.confirm ("Click OK to confirm if you want to include numeric characters.")) {
+    userChoices.concat(numberChars);
+  } else {
+      noCounter++;
+  } 
+    if (window.confirm ("Click OK to confirm if you want to include uppercase characters.")) {
+    userChoices.concat(uppercase);
+  } else {
+    noCounter++;
+  }
+    if (window.confirm ("Click OK to confirm if you want to include lowercase characters.")) {
+    userChoices.concat(lowercase);
+  } else {
+    noCounter++;
+  }
+    if (noCounter==4) {
+      window.alert("You must choose at least one Character."); 
+      return;
+    }
+
 return userChoices;
 };
 
@@ -46,53 +56,31 @@ return userChoices;
 
 function randomSelected () {
  var randomChoice = userSelected();
- randomChoice[Math.floor(Math.random()*randomChoice.length)];
- 
- for (var i = 0; i < randomChoice.length; i++) {
-   var randomNum = randomChoice[i];
-   
- }
+ var storeRandomLetters = [];
 
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNum = randomChoice[Math.floor(Math.random()*randomChoice.length)];
+    storeRandomLetters = storeRandomLetters.concat(randomChoice[randomNum]);
+  }
+ 
+//  for (var i = 0; i < randomChoice.length; i++) {
+//    //picking a random number from array 
+//    var randomNum = randomChoice[Math.floor(Math.random()*randomChoice.length)];
+//   storeRandomLetters = storeRandomLetters.concat(randomChoice[randomNum]);
+
+ storeRandomLetters = storeRandomLetters.join("");
+return storeRandomLetters;
 }
 
 //function to generate the password from user input 
 function generatePassword () {
+  var storePassword = randomSelected();
+  return storePassword;
   
-  var storePassword = userSelected();
-  //create an empty variable to store the concatenated password
-  var possibleChar = userSelected();
-  // create an empty variable holding all possible password choices
-  var pickedChar = userSelected();
-  // create an empty variable to hold the chosen characters 
 
-  //error handling to see if the object with your choices exist or not
-
-  //conditional statements are needed to add if the user has chosen special chracters into an array of possible charatcers and must push new random characters to chosen array
-  //use concatenate method
-  if (userSelected.userchoices == specialChars) {
-    possibleChar
-  
-  }
   
 };
 
-// iterate over the password length from the choice made (obj) selecting random indexes from the array of possible characters and outs them into a result variable
-// for (var i = 0; i < choice.length; i++) {
-//   var possibleChar = getSelection[i];
-  
-// }
-// // ix in at least one of the chosen characters and the result 
-// for (let index = 0; index < array.length; index++) {
-//   const element = array[index];
-  
-  //JOIN the results and send them to the writePassword function to present on the page }
-
-
-
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 
 
@@ -117,3 +105,18 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+  // var storePassword = randomSelected();
+  // //create an empty variable to store the concatenated password
+  // var possibleChar = userSelected();
+  // // create an empty variable holding all possible password choices
+  // var pickedChar = userSelected();
+  // // create an empty variable to hold the chosen characters 
+
+  // //error handling to see if the object with your choices exist or not
+
+  // //conditional statements are needed to add if the user has chosen special chracters into an array of possible charatcers and must push new random characters to chosen array
+  // //use concatenate method
+  // if (userSelected.userchoices == specialChars) {
+  //   possibleChar
+  
+  // }
